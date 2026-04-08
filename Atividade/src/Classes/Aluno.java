02f922ab643fed2c33b676ec8dffc3f15c933a18
Ferdinando Rainert <nandorainert2004@gmail.com>
2026-04-08 11:07:33 -0300

    Código finalizado

--- Atividade/src/Classes/Aluno.java
+++ Atividade/src/Classes/Aluno.java
@@ -1,5 +1,67 @@
 package Classes;
 
 public class Aluno {
+	
+	private String nome;
+	private String disciplina;
+	private double nota;
+	private double media;
+	
+	public Aluno(String nome, String disciplina, double nota) {
+		this.nome = nome;
+		this.disciplina = disciplina;
+		this.nota = nota;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = validaNota(nota);
+	}
+	
+	public double getMedia() {
+		return media/3;
+	}
+
+	public void setMedia(double media) {
+		this.media = media;
+	}
+	
+	public double validaNota(double nota) {
+		
+		if(nota > 10){
+			
+			nota = 10;
+			
+		}else if(nota < 0){
+			
+			nota = 0;
+		}
+		
+		return nota;
+	}
+
+	public void info() {
+		System.out.println("Nome do aluno: " + getNome());
+		System.out.println("Nome da disciplina: " + getDisciplina());
+		System.out.println("Média final: " + getMedia());
+	}
 }
--- Atividade/src/main/Main.java
+++ Atividade/src/main/Main.java
@@ -1,10 +1,47 @@
 package main;
 
+import java.util.Scanner;
+
+import Classes.Aluno;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Aluno aluno = new Aluno ("Ferdinando", "POO", 10.00);
+		
+		double nota1;
+		double nota2;
+		double nota3;
+		
+		System.out.println("Digite o nome do aluno: ");
+		String nome = sc.nextLine();
+		System.out.println("Digite o nome da disciplina: ");
+		String disciplina = sc.nextLine();
+		
+        Aluno aluno1 = new Aluno(nome, disciplina, 0);
+        
+        System.out.print("Digite a nota 1: ");
+        nota1 = sc.nextDouble();
+        
+        System.out.print("Digite a nota 2: ");
+        nota2 = sc.nextDouble();
+        
+        System.out.print("Digite a nota 3: ");
+        nota3 = sc.nextDouble();
+        
+        aluno1.setMedia(
+            aluno1.validaNota(nota1) +
+            aluno1.validaNota(nota2) +
+            aluno1.validaNota(nota3)
+        );
+        
+        aluno1.info();
+        
+        sc.close();
+        
 	}
 
 }
